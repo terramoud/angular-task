f9230083b056a82feeaf076174b3f3ea4d089552
Oleksandr Koreshev <a.koreshev1@gmail.com>
2023-07-21 23:00:38 +0300

    add api service

---
+++ src/app/api.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  private apiUrl = 'https://api.example.com';
+
+  constructor(private http: HttpClient) { }
+
+  getData(): Promise<any> {
+    return this.http.get<any>(`${this.apiUrl}/data`).toPromise();
+  }
+}
+
