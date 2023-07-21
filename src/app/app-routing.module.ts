7b994523b262f908a8259d87847b4c7c35a3204e
Oleksandr Koreshev <a.koreshev1@gmail.com>
2023-07-21 19:51:32 +0300

    create root component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import {MainPageComponent} from "./main-page/main-page.component";
+import {RegistrationPageComponent} from "./registration-page/registration-page.component";
+
+const routes: Routes = [
+  { path: '', redirectTo: '/main', pathMatch: 'full' },
+  { path: 'register', component: RegistrationPageComponent },
+  { path: 'main', component: MainPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
