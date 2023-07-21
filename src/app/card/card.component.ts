a2b4684d6fe50f8d556bd08d4b61d0dfaec85a03
Oleksandr Koreshev <a.koreshev1@gmail.com>
2023-07-21 19:52:00 +0300

    create card component

---
+++ src/app/card/card.component.ts
@@ -0,0 +1,15 @@
+import {Component, Input} from '@angular/core';
+import {Card} from "../main-page/card";
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.scss']
+})
+export class CardComponent {
+  @Input() card: Card;
+
+  constructor() {
+    this.card = new Card();
+  }
+}
---
+++ src/app/main-page/card.ts
@@ -0,0 +1,75 @@
+export class Card {
+  private name: string;
+  private description: string;
+  private expireTime: string;
+  private price: string;
+  private createdDate: number;
+  private category: string;
+  private pageNumber: number;
+
+  constructor() {
+    this.name = "";
+    this.description = "";
+    this.expireTime = "";
+    this.price = "";
+    this.createdDate = 0;
+    this.category = "";
+    this.pageNumber = 0;
+  }
+
+  public getName(): string {
+    return this.name;
+  }
+
+  public setName(value: string): void  {
+    this.name = value;
+  }
+
+  public getDescription(): string {
+    return this.description;
+  }
+
+  public setDescription(value: string): void  {
+    this.description = value;
+  }
+
+  public getExpireTime(): string {
+    return this.expireTime;
+  }
+
+  public setExpireTime(value: string): void  {
+    this.expireTime = value;
+  }
+
+  public getPrice(): string {
+    return this.price;
+  }
+
+  public setPrice(value: string): void  {
+    this.price = value;
+  }
+
+  public getCreatedDate(): number {
+    return this.createdDate;
+  }
+
+  public setCreatedDate(value: number): void  {
+    this.createdDate = value;
+  }
+
+  public getCategory(): string {
+    return this.category;
+  }
+
+  public setCategory(value: string): void  {
+    this.category = value;
+  }
+
+  public getPageNumber(): number {
+    return this.pageNumber;
+  }
+
+  public setPageNumber(value: number): void {
+    this.pageNumber = value;
+  }
+}
